438ef4959aaa52cbc65787b2352accfd096fcec2
Vaidant Jadham <vaidantjadham24@gmail.com>
2025-03-03 13:50:20 +0530

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="text-white text-2xl">
+        <h1>Movie Empire</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
